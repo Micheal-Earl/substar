53a093f82eb1db596928336b623b8d0d026f62ca
Micheal Earl <michealearldev@gmail.com>
2024-08-29 19:24:08 -0700

    Project revived, FastHTML

--- src/routes/api/test/[id].ts
+++
@@ -1,7 +0,0 @@
-import { APIEvent } from "solid-start/api";
-
-export async function GET({ params }: APIEvent) {
-  const id = params.id;
-  const res = await fetch(`https://dummyjson.com/products/${id}`);
-  return res;
-}
--- src/routes/api/test/echo.ts
+++
@@ -1,6 +0,0 @@
-import { APIEvent, json } from "solid-start/api";
-
-export async function POST({ request }: APIEvent) {
-  const jsonRequest = await request.json();
-  return json(jsonRequest);
-}
